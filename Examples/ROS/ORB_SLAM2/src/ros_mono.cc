4f48c06cebf0004630dc8bd47f0d4a6a06ee1607
Justin Lin <tzuyuan@umich.edu>
2021-07-20 15:44:12 -0400

    modify ORB SLAM2 for D455

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     ImageGrabber igb(&SLAM);
 
     ros::NodeHandle nodeHandler;
-    ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw", 1, &ImageGrabber::GrabImage,&igb);
+    ros::Subscriber sub = nodeHandler.subscribe("/camera/color/image_raw", 1, &ImageGrabber::GrabImage,&igb);
 
     ros::spin();
 
@@ -69,7 +69,7 @@ int main(int argc, char **argv)
     SLAM.Shutdown();
 
     // Save camera trajectory
-    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+    SLAM.SaveKeyFrameTrajectoryTUM("mono_KeyFrameTrajectory.txt");
 
     ros::shutdown();
 
---
+++ script/run_rgbd.bash
@@ -0,0 +1,4 @@
+voc_path="/media/curly_ssd_justin/code/ORB_SLAM2/Vocabulary/ORBvoc.txt"
+config_path="/media/curly_ssd_justin/code/ORB_SLAM2/Examples/RGB-D/D455.yaml"
+
+rosrun ORB_SLAM2 RGBD $voc_path $config_path
